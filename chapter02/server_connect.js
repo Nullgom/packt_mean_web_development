c4735cd13874296ea7bc62099311703faa7387bf
nullgom <surfwon@gmail.com>
2017-07-02 22:51:34 +0900

    로그함수 미들웨어 등록

--- chapter02/server_connect.js
+++ chapter02/server_connect.js
@@ -1,11 +1,19 @@
 const connect = require('connect');
 const app = connect();
 
+// 로그 출력 함수
+function logger(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+};
+
 function helloWorld(req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
 };
 
+// 로그 출력 함수 미들웨어 등록
+app.use(logger);
 app.use(helloWorld);
 
 app.listen(3000);
