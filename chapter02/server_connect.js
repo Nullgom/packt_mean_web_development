7d6a3db320e04779bcfb20854f07cc754a335a79
nullgom <surfwon@gmail.com>
2017-07-02 22:57:24 +0900

    패스에 따른 함수 등록

--- chapter02/server_connect.js
+++ chapter02/server_connect.js
@@ -12,9 +12,17 @@ function helloWorld(req, res, next) {
     res.end('Hello World');
 };
 
+function goodbyeWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h2>Goodbye World!</h2>');
+}
+
 // 로그 출력 함수 미들웨어 등록
 app.use(logger);
-app.use(helloWorld);
+
+// 패스에 따른 처리 함수 등록.
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbyeWorld);
 
 app.listen(3000);
 console.log('Server running at http://localhost:3000');
